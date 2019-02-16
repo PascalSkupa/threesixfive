0862d67ded720252952ef234f57f412a8a8679f6
Pascal Skupa <pascal.skupa@htl.rennweg.at>
2019-02-16 16:43:00 +0100

    Pre Merge

---
+++ src/app/food-formular/food-formular.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {first} from 'rxjs/operators';
+import {Router} from '@angular/router';
+import {SelectItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-food-formular',
+  templateUrl: './food-formular.component.html',
+  styleUrls: ['./food-formular.component.scss']
+})
+export class FoodFormularComponent implements OnInit {
+
+  types: SelectItem[];
+
+  selectedType: string;
+
+  selectedTypes: string[] = ['PayPal', 'MasterCard'];
+
+  selectedModes: string[];
+
+  modes: SelectItem[];
+
+  countries: any[];
+
+  selectedCountry: any;
+
+
+  constructor(private router: Router) {
+
+    this.types = [
+      {label: 'Paypal', value: 'PayPal', icon: 'fa fa-fw fa-cc-paypal'},
+      {label: 'Visa', value: 'Visa', icon: 'fa fa-fw fa-cc-visa'},
+      {label: 'MasterCard', value: 'MasterCard', icon: 'fa fa-fw fa-cc-mastercard'}
+    ];
+
+    this.modes = [
+      {value: 'Bold', title: 'Bold', icon: 'fa fa-fw fa-bold'},
+      {value: 'Italic', title: 'Italic', icon: 'fa fa-fw fa-italic'},
+      {value: 'Underline', title: 'Underline', icon: 'fa fa-fw fa-underline'}
+    ];
+
+    this.countries = [
+      {name: 'USA', flag: 'usa.png'},
+      {name: 'Germany', flag: 'germany.png'},
+      {name: 'Japan', flag: 'japan.png'}
+    ];
+  }
+
+  clear() {
+    this.selectedType = null;
+    this.selectedTypes = [];
+    this.selectedModes = [];
+    this.selectedCountry = null;
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.router.navigate(['/']);
+  }
+
+
+}
