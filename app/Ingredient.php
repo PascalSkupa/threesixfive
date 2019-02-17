df73922169b7bcfd4b94ebdf247bbde3082a5e30
Christian Perl <christian.perl@htl.rennweg.at>
2019-02-17 11:19:29 +0100

    Programming classes Recipe.php and Ingredient.php

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -6,4 +6,51 @@ namespace App;
 class Ingredient
 {
 
+    private $id;
+
+    private $name;
+
+    private $sub_categories;
+
+    private $servings;
+
+    public function __construct($ingredient)
+    {
+        $this->id = $ingredient['food_id'];
+        $this->name = $ingredient['food_name'];
+        $this->sub_categories = $ingredient['food_sub_categories']['food_sub_category'];
+        $this->servings = $ingredient['servings']['serving'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubCategories()
+    {
+        return $this->sub_categories;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getServings()
+    {
+        return $this->servings;
+    }
 }
--- app/Recipe.php
+++ app/Recipe.php
@@ -6,4 +6,96 @@ namespace App;
 class Recipe
 {
 
+    private $id;
+
+    private $name;
+
+    private $description;
+
+    private $types;
+
+    private $categories;
+
+    private $serving;
+
+    private $ingredients;
+
+    private $direction;
+
+    public function __construct($recipe)
+    {
+        $this->id = $recipe['recipe_id'];
+        $this->name = $recipe['recipe_name'];
+        $this->description = $recipe['recipe_description'];
+        $this->types = $recipe['recipe_types']['recipe_type'];
+        $this->categories = $recipe['recipe_categories']['recipe_category'];
+        $this->serving = $recipe['number_of_servings'];
+        $this->direction = $recipe['directions']['direction'];
+
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypes()
+    {
+        return $this->types;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getServing()
+    {
+        return $this->serving;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIngredients()
+    {
+        return $this->ingredients;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDirection()
+    {
+        return $this->direction;
+    }
 }
