d4cc17c0709b38d580d15c8892773164450d7561
Marwan Abdalla <4160@htl.rennweg,at>
2019-03-12 12:11:14 +0100

    userprofile workds

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-
-  constructor() { }
+  emailEdit = false;
+  firstnameEdit = false;
+  lastNameEdit = false;
+  firstnameValue = 'Marwan';
+  lastnameValue = 'Abdalla';
+  mailValue = '4160@htl.rennweg.at';
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  changeMail() {
+    this.emailEdit = true;
+  }
+
+  changeFisrtname() {
+    this.firstnameEdit = true;
+  }
+
+  changeLastname() {
+    this.lastNameEdit = true;
+  }
+  saveMail() {
+    this.emailEdit = false;
+  }
+  saveFirstname() {
+    this.firstnameEdit = false;
+  }
+  saveLastname() {
+    this.lastNameEdit = false;
+  }
 }
